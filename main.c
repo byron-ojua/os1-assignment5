db6b1fe3b37f1f168c2415ab0daa2fb6cd5eb2f2
Byron OjuaNice <niceb@os1.engr.oregonstate.edu>
2023-03-16 18:40:33 -0700

    Added debug comments

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ struct dataBuffer buffer_1;
 pthread_mutex_t mutex_1 = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t full_1 = PTHREAD_COND_INITIALIZER;
 
+
 // Buffer 2, shared resource between separator thread and the plus thread
 struct dataBuffer buffer_2;
 pthread_mutex_t mutex_2 = PTHREAD_MUTEX_INITIALIZER;
@@ -24,10 +25,16 @@ pthread_cond_t full_3 = PTHREAD_COND_INITIALIZER;
  * @param buff buffer to copy from
  */
 void put_buff_1(struct dataBuffer *buff){
+    fprintf(stderr, "put_buff_1: Locking mutex_1\n");
+    fflush(stderr);
     pthread_mutex_lock(&mutex_1);
     strcpy(buffer_1.data, buff->data);
     buffer_1.size = buff->size;
+    fprintf(stderr, "put_buff_1: Signal full_1\n");
+    fflush(stderr);
     pthread_cond_signal(&full_1);
+    fprintf(stderr, "put_buff_1: Unlocking mutex_1\n");
+    fflush(stderr);
     pthread_mutex_unlock(&mutex_1);
 }
 
@@ -37,10 +44,16 @@ void put_buff_1(struct dataBuffer *buff){
  * @param buff buffer to copy from
  */
 void put_buff_2(struct dataBuffer *buff){
+    fprintf(stderr, "put_buff_2: Locking mutex_2\n");
+    fflush(stderr);
     pthread_mutex_lock(&mutex_2);
     strcpy(buffer_2.data, buff->data);
     buffer_2.size = buff->size;
+    fprintf(stderr, "put_buff_2: Signal full_2\n");
+    fflush(stderr);
     pthread_cond_signal(&full_2);
+    fprintf(stderr, "put_buff_2: Unlocking mutex_2\n");
+    fflush(stderr);
     pthread_mutex_unlock(&mutex_2);
 }
 
@@ -50,10 +63,16 @@ void put_buff_2(struct dataBuffer *buff){
  * @param buff buffer to copy from
  */
 void put_buff_3(struct dataBuffer *buff){
+    fprintf(stderr, "put_buff_3: Locking mutex_3\n");
+    fflush(stderr);
     pthread_mutex_lock(&mutex_3);
     strcpy(buffer_3.data, buff->data);
     buffer_3.size = buff->size;
+    fprintf(stderr, "put_buff_3: Signal full_3\n");
+    fflush(stderr);
     pthread_cond_signal(&full_3);
+    fprintf(stderr, "put_buff_3: Unlocking mutex_3\n");
+    fflush(stderr);
     pthread_mutex_unlock(&mutex_3);
 }
 
@@ -63,12 +82,22 @@ void put_buff_3(struct dataBuffer *buff){
  * @param buff buffer to copy into
  */
 void get_buff_1(struct dataBuffer *buff){
+    fprintf(stderr, "get_buff_1: Locking mutex_1\n");
+    fflush(stderr);
     pthread_mutex_lock(&mutex_1);
+    fprintf(stderr, "get_buff_1: Entering cond_wait\n");
+    fflush(stderr);
+
     while (buffer_1.size == 0){
         pthread_cond_wait(&full_1, &mutex_1);
     }
+    fprintf(stderr, "get_buff_1: Leaving cond_wait\n");
+    fflush(stderr);
+
     strcpy(buff->data, buffer_1.data);
     buff->size = buffer_1.size;
+    fprintf(stderr, "get_buff_1: Unlocking mutex_1\n");
+    fflush(stderr);
     pthread_mutex_unlock(&mutex_1);
 }
 
@@ -78,14 +107,22 @@ void get_buff_1(struct dataBuffer *buff){
  * @param buff buffer to copy into
  */
 void get_buff_2(struct dataBuffer *buff){
+    fprintf(stderr, "get_buff_2: Locking mutex_2\n");
+    fflush(stderr);
     pthread_mutex_lock(&mutex_2);
 
+    fprintf(stderr, "get_buff_2: Entering cond_wait\n");
+    fflush(stderr);
     while (buffer_2.size == 0){
         pthread_cond_wait(&full_2, &mutex_2);
     }
+    fprintf(stderr, "get_buff_2: Leaving cond_wait\n");
+    fflush(stderr);
 
     strcpy(buff->data, buffer_2.data);
     buff->size = buffer_2.size;
+    fprintf(stderr, "get_buff_2: Unlocking mutex_2\n");
+    fflush(stderr);
     pthread_mutex_unlock(&mutex_2);
 }
 
@@ -95,14 +132,23 @@ void get_buff_2(struct dataBuffer *buff){
  * @param buff buffer to copy into
  */
 void get_buff_3(struct dataBuffer *buff){
+    fprintf(stderr, "get_buff_3: Locking mutex_3\n");
+    fflush(stderr);
     pthread_mutex_lock(&mutex_3);
 
+    fprintf(stderr, "get_buff_3: Entering cond_wait\n");
+    fflush(stderr);
     while (buffer_1.size == 0){
         pthread_cond_wait(&full_3, &mutex_3);
     }
+    fprintf(stderr, "get_buff_3: Leaving cond_wait\n");
+    fflush(stderr);
 
     strcpy(buff->data, buffer_3.data);
     buff->size = buffer_3.size;
+
+    fprintf(stderr, "get_buff_3: Unlocking mutex_3\n");
+    fflush(stderr);
     pthread_mutex_unlock(&mutex_3);
 }
 
@@ -112,11 +158,11 @@ void get_buff_3(struct dataBuffer *buff){
  * @param buff buffer to copy input data into
  */
 void get_user_input(struct dataBuffer *buff){
-    for(int i = 0; i < 1000; i++){
+    for(int i = 0; i < MAX_LINE_LENGTH; i++){
         buff->data[i] = fgetc(stdin);
-        if(buff->data[i] == EOF || (buff->data[i] == LINE_SEPARATOR && buff->data[i-1] == 'P' && buff->data[i-2] == 'O' && buff->data[i-3] == 'T' && buff->data[i-4] == 'S' && buff->data[i-5] == LINE_SEPARATOR)){
-            for(int j = i-5; j < i; j++){
-                buff->data[j] = '\0';
+        if(buff->data[i] == EOF || (buff->data[i] == LINE_SEPARATOR && buff->data[i-4] == 'S' && buff->data[i-3] == 'T' && buff->data[i-2] == 'O' && buff->data[i-1] == 'P' && buff->data[i-5] == LINE_SEPARATOR)){
+            for(int offset = i-5; offset < i; offset++){
+                buff->data[offset] = '\0';
             }
 
             buff->size = i - 5;
@@ -176,8 +222,8 @@ void *plus_thread(void *arg) {
         if(buffer.data[i] == '+' && buffer.data[i+1] == '+'){
             buffer.data[i] = '^';
             buffer.data[i+1] = '\0';
-            for(int j = i+1; j < buffer.size; j++){
-                buffer.data[j] = buffer.data[j+1];
+            for(int offset = i + 1; offset < buffer.size; offset++){
+                buffer.data[offset] = buffer.data[offset + 1];
             }
         }
     }
@@ -195,23 +241,22 @@ void *plus_thread(void *arg) {
 void *output_thread(void *arg) {
     struct dataBuffer buffer;
     buffer.size = 0;
-    char *temp = calloc(80, sizeof(char));
+    char *printBuff = calloc(80, sizeof(char));
     get_buff_3(&buffer);
 
     for(int i = 0; i < buffer.size; i++){
         if(i % 80 == 0 && i != 0){
-            printf("%s", temp);
+            printf("%s", printBuff);
             printf("\n");
             fflush(stdout);
         }
-        temp[i % 80] = buffer.data[i];
+        printBuff[i % 80] = buffer.data[i];
     }
 
-    free(temp);
+    free(printBuff);
 }
 
 int main(int argc, char *argv[]){
-    srand(time(0));
     pthread_t threads[4];
     pthread_create(&threads[0], NULL, input_thread, NULL);
     pthread_create(&threads[1], NULL, separator_thread, NULL);
